7a3e8cf49027d0c3d54f87a8d8f5f39e9bfae23e
seopmin <alstjqrhkr2018@gmail.com>
2024-01-30 00:49:34 +0900

    json 외부 라이브러리 추가

--- app/main.cc
+++ app/main.cc
@@ -1,10 +1,16 @@
 #include <iostream>
 #include "my_lib.hpp"
 #include "config.hpp"
+#include <nlohmann/json.hpp>
 
 int main() {
   std::cout << project_name << "\n";
   std::cout << project_version << "\n";
+
+  std::cout << "JSON Lib Version:"
+    << NLOHMANN_JSON_VERSION_MAJOR << "."
+    << NLOHMANN_JSON_VERSION_MINOR << "."
+    << NLOHMANN_JSON_VERSION_PATCH << "\n";
 
   print_hello_world();
 
--- cmake/AddGitSubmoduleFile.cmake
+++ cmake/AddGitSubmoduleFile.cmake
@@ -1,4 +1,21 @@
-# function(add_git_submodule dir)
-#   find
+# cmake function 구현
+
+# function('func name' '매개변수')
+function(add_git_submodule dir)
+	# git이 없으면 cmake 실패
+  find_package(Git REQUIRED)
   
-# endfunction(add_git_submodule)
+	# cmake가 없으면 해당 dir에서 초기화 하고 업데이트 시행
+	if (NOT EXISTS ${CMAKE_SOURCE_DIR}/${dir}/CMakeLists.txt)
+		execute_process(COMMAND ${GIT_EXECUTABLE}
+			submodule update --init --recursive -- ${CMAKE_SOURCE_DIR}/${dir}
+			WORKING_DIRECTORY ${PROJECT_SOURCE_DIR})
+	endif()
+
+	if (EXISTS ${CMAKE_SOURCE_DIR}/${dir}/CMakeLists.txt)
+		message("Adding: ${CMAKE_SOURCE_DIR}/${dir}/CMakeLists.txt")
+		add_subdirectory(${CMAKE_SOURCE_DIR}/${dir})
+	else()
+		message("Could not add: ${CMAKE_SOURCE_DIR}/${dir}/CMakeLists.txt")
+	endif()
+endfunction(add_git_submodule)
